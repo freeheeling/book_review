15247ff2fd41ea705d3c89048d215f843d2e3d96
Scott Regenthal <scottregenthal@gmail.com>
2020-02-21 10:22:42 -0700

    Modify view and begin second API call within controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,23 @@ class SearchController < ApplicationController
   def index
     title = params[:title]
 
-    conn = Faraday.new(url: 'http://openlibrary.org') do |f|
+    ol_conn = Faraday.new(url: 'http://openlibrary.org') do |f|
       f.adapter Faraday.default_adapter
       f.params['title'] = title
     end
 
-    response = conn.get('/search.json')
+    ol_response = ol_conn.get('/search.json')
+
+    ol_parsed_json = JSON.parse(ol_response.body, symbolize_names: true)
+    @books = ol_parsed_json[:docs].first
+
+    nyt_conn = Faraday.new(url: 'https://api.nytimes.com/') do |f|
+      f.adapter Faraday.default_adapter
+      f.params['api-key'] = ENV['NYT_KEY']
+      f.params['title'] = title
+    end
+
+    nyt_response = nyt_conn.get('svc/books/v3/reviews.json')
 
     require "pry"; binding.pry
   end
